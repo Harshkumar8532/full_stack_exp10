2d98b706f6af38ef7c7632b7f5caa4aa29795911
Harsh kumar <sainikrishna8338@gmail.com>
2026-04-15 00:53:28 +0530

    final commit

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,4 +1,4 @@
-cdconst express = require('express');
+const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order');
 
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
